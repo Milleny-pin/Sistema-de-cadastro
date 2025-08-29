60e10d3d73975e3bfb8a4e5956441c38d7b54a69
Milleny Souza Braghine <Millenybragh@gmail.com>
2025-08-28 22:06:52 -0300

    Primeiros passos conexão com mongo db e prisma

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express' 
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const app = express()
+
+const users = []
+
+app.post('/usuarios',async (req, res)=>{
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    })
+
+app.get('/usuarios', (request, response)=> {
+    response.send('Olá')
+}) 
+
+app.listen(3000)
